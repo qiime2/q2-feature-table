f58440cc819ce44d2f0eb83ac9d39f3bdfd24d11
patthehat033 <maximumamps@gmail.com>
2017-12-11 02:46:23 +0000

    Added validation code to correct if/else branch

--- q2_feature_table/_summarize/summarize_assets/src/controls.js
+++ q2_feature_table/_summarize/summarize_assets/src/controls.js
@@ -61,15 +61,14 @@ const initializeControls = () => {
     if (+sliderValue.node().value > +slider.node().max) {
       sliderValue.node().value = slider.node().max;
       slider.node().value = slider.node().max;
-    } else {
-      slider.node().value = sliderValue.node().value;
     }
-
-    if (sliderValue.node().value < 0) {
+    else if (sliderValue.node().value < 0) {
       sliderValue.node().value = 0;
       slider.node().value = slider.node().min;
     }
-
+    else {
+      slider.node().value = sliderValue.node().value;
+    }
     updateStats(trows, slider);
     toggleStyles(trows, slider);
     hiddenDepth.attr('value', slider.node().value).dispatch('change');
