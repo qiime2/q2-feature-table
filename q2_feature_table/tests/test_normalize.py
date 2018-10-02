4c044a227f029d7c4c0b1e5a20d190546f9b75d0
Steven Brown <sdbrown@chelonioidea.local>
2018-10-02 15:00:39 -0700

    add rarefy with replacement

--- q2_feature_table/tests/test_normalize.py
+++ q2_feature_table/tests/test_normalize.py
@@ -27,6 +27,13 @@ class RarefyTests(TestCase):
         self.assertEqual(set(a.ids(axis='observation')), set(['O1', 'O2']))
         npt.assert_array_equal(a.sum(axis='sample'), np.array([2., 2.]))
 
+    def test_rarefy_replacement(self):
+        t = Table(np.array([[0, 1, 3], [1, 1, 2]]),
+                  ['O1', 'O2'],
+                  ['S1', 'S2', 'S3'])
+         rt = rarefy(t, 3, with_replacement=True)
+        self.assertEqual(rt.shape, (2, 3))
+
     def test_rarefy_depth_error(self):
         t = Table(np.array([[0, 1, 3], [1, 1, 2]]),
                   ['O1', 'O2'],
