0559abc8a2e72834acdbdfc94cdb152e63f1c4ad
Matthew Dillon <matthewrdillon@gmail.com>
2019-05-02 15:24:24 -0700

    SQUASH: standardize single quotes

--- q2_feature_table/_summarize/_vega_spec.py
+++ q2_feature_table/_summarize/_vega_spec.py
@@ -8,7 +8,6 @@
 
 
 def vega_spec(sample_metadata, sample_frequencies):
-
     values = []
 
     sample_metadata = sample_metadata.filter_ids(
@@ -26,290 +25,290 @@ def vega_spec(sample_metadata, sample_frequencies):
     max_frequency = int(max(sample_frequencies.values.tolist()))
 
     spec = {
-            "$schema": "https://vega.github.io/schema/vega/v4.json",
-            "autosize": {"contains": "content",
-                         "type": "fit-x",
-                         "resize": True},
-            "width": 800,
-            "data": [
+            '$schema': 'https://vega.github.io/schema/vega/v4.json',
+            'autosize': {'contains': 'content',
+                         'type': 'fit-x',
+                         'resize': True},
+            'width': 800,
+            'data': [
                 {
-                  "transform": [
+                  'transform': [
                     {
-                      "as": "selectedCategory",
-                      "expr": "datum.metadata[category]",
-                      "type": "formula"
+                      'as': 'selectedCategory',
+                      'expr': 'datum.metadata[category]',
+                      'type': 'formula'
                     }
                   ],
-                  "name": "table",
-                  "values": values
+                  'name': 'table',
+                  'values': values
                 },
                 {
-                  "transform": [
+                  'transform': [
                     {
-                      "type": "aggregate",
-                      "groupby": [
-                        "selectedCategory"
+                      'type': 'aggregate',
+                      'groupby': [
+                        'selectedCategory'
                       ]
                     }
                   ],
-                  "name": "groupedBySelectedCategory",
-                  "source": "table"
+                  'name': 'groupedBySelectedCategory',
+                  'source': 'table'
                 },
                 {
-                  "transform": [
+                  'transform': [
                     {
-                      "expr": "datum.frequency >= samplingDepth",
-                      "type": "filter"
+                      'expr': 'datum.frequency >= samplingDepth',
+                      'type': 'filter'
                     },
                     {
-                      "type": "aggregate",
-                      "groupby": [
-                        "selectedCategory"
+                      'type': 'aggregate',
+                      'groupby': [
+                        'selectedCategory'
                       ]
                     }
                   ],
-                  "name": "groupedBySelectedCategoryAndRetained",
-                  "source": "table"
+                  'name': 'groupedBySelectedCategoryAndRetained',
+                  'source': 'table'
                 }
               ],
-            "signals": [
+            'signals': [
                     {'name': 'width', 'value': '', 'bind': {'input': 'text'},
                              'on': [{'events': {'source': 'window',
                                                 'type': 'resize'},
                                      'update': 'containerSize()[0]'}]},
-                    {"name": "chartHeight", "update": "width / 2.5"},
-                    {"name": "chartOffset", "value": 20},
-                    {"name": "height", "update": "chartHeight + chartOffset"},
+                    {'name': 'chartHeight', 'update': 'width / 2.5'},
+                    {'name': 'chartOffset', 'value': 20},
+                    {'name': 'height', 'update': 'chartHeight + chartOffset'},
                     {
-                      "name": "tooltip",
-                      "value": {},
-                      "on": [
+                      'name': 'tooltip',
+                      'value': {},
+                      'on': [
                         {
-                          "events": "rect:mouseover",
-                          "update": "datum"
+                          'events': 'rect:mouseover',
+                          'update': 'datum'
                         },
                         {
-                          "events": "rect:mouseout",
-                          "update": "{}"
+                          'events': 'rect:mouseout',
+                          'update': '{}'
                         }
                       ]
                     },
                     {
-                      "bind": {
-                        "input": "select",
-                        "options": metadata_categories,
-                        "element": "#metadata-category"
+                      'bind': {
+                        'input': 'select',
+                        'options': metadata_categories,
+                        'element': '#metadata-category'
                       },
-                      "name": "category",
-                      "value": metadata_categories[0]
+                      'name': 'category',
+                      'value': metadata_categories[0]
                     },
                     {
-                      "bind": {
-                        "input": "range",
-                        "element": "#sampling-depth-slider",
-                        "max": max_frequency,
-                        "min": 0,
-                        "step": 1
+                      'bind': {
+                        'input': 'range',
+                        'element': '#sampling-depth-slider',
+                        'max': max_frequency,
+                        'min': 0,
+                        'step': 1
                       },
-                      "name": "samplingDepth",
-                      "value": 0
+                      'name': 'samplingDepth',
+                      'value': 0
                     }
                   ],
-            "marks": [
+            'marks': [
                 {
-                  "description": "Interactive rarefaction summary plot",
-                  "name": "rarefactionplot",
-                  "type": "group",
-                  "encode": {
-                    "enter": {
-                      "x": {
-                        "value": 0
+                  'description': 'Interactive rarefaction summary plot',
+                  'name': 'rarefactionplot',
+                  'type': 'group',
+                  'encode': {
+                    'enter': {
+                      'x': {
+                        'value': 0
                       },
-                      "y": {
-                        "signal": "chartOffset"
+                      'y': {
+                        'signal': 'chartOffset'
                       },
-                      "width": {
-                        "value": 200
+                      'width': {
+                        'value': 200
                       },
-                      "height": {
-                        "signal": "chartHeight"
+                      'height': {
+                        'signal': 'chartHeight'
                       }
                     }
                   },
-                  "marks": [
+                  'marks': [
                     {
-                      "type": "rect",
-                      "from": {
-                        "data": "groupedBySelectedCategory"
+                      'type': 'rect',
+                      'from': {
+                        'data': 'groupedBySelectedCategory'
                       },
-                      "encode": {
-                        "enter": {
-                          "x": {
-                            "scale": "xscale",
-                            "field": "selectedCategory"
+                      'encode': {
+                        'enter': {
+                          'x': {
+                            'scale': 'xscale',
+                            'field': 'selectedCategory'
                           },
-                          "width": {
-                            "scale": "xscale",
-                            "band": 1
+                          'width': {
+                            'scale': 'xscale',
+                            'band': 1
                           },
-                          "y": {
-                            "scale": "yscale",
-                            "field": "count"
+                          'y': {
+                            'scale': 'yscale',
+                            'field': 'count'
                           },
-                          "y2": {
-                            "scale": "yscale",
-                            "value": 0
+                          'y2': {
+                            'scale': 'yscale',
+                            'value': 0
                           }
                         },
-                        "update": {
-                          "tooltip": {
-                            "signal": (
-                                "{\"title\": datum.selectedCategory, "
-                                "\"Samples Dropped\": datum.count}")
+                        'update': {
+                          'tooltip': {
+                            'signal': (
+                                '{\'title\': datum.selectedCategory, '
+                                '\'Samples Dropped\': datum.count}')
                           },
-                          "fill": {
-                            "value": "#D3D3D3"
+                          'fill': {
+                            'value': '#D3D3D3'
                           }
                         }
                       }
                     },
                     {
-                      "type": "rect",
-                      "from": {
-                        "data": "groupedBySelectedCategoryAndRetained"
+                      'type': 'rect',
+                      'from': {
+                        'data': 'groupedBySelectedCategoryAndRetained'
                       },
-                      "encode": {
-                        "enter": {
-                          "x": {
-                            "scale": "xscale",
-                            "field": "selectedCategory"
+                      'encode': {
+                        'enter': {
+                          'x': {
+                            'scale': 'xscale',
+                            'field': 'selectedCategory'
                           },
-                          "width": {
-                            "scale": "xscale",
-                            "band": 1
+                          'width': {
+                            'scale': 'xscale',
+                            'band': 1
                           },
-                          "y": {
-                            "scale": "yscale",
-                            "field": "count"
+                          'y': {
+                            'scale': 'yscale',
+                            'field': 'count'
                           },
-                          "y2": {
-                            "scale": "yscale",
-                            "value": 0
+                          'y2': {
+                            'scale': 'yscale',
+                            'value': 0
                           }
                         },
-                        "update": {
-                          "tooltip": {
-                            "signal": (
-                              "{\"title\": datum.selectedCategory, "
-                              "\"Samples Retained\": datum.count}")
+                        'update': {
+                          'tooltip': {
+                            'signal': (
+                              '{\'title\': datum.selectedCategory, '
+                              '\'Samples Retained\': datum.count}')
                               },
-                          "y": {
-                            "scale": "yscale",
-                            "field": "count"
+                          'y': {
+                            'scale': 'yscale',
+                            'field': 'count'
                           },
-                          "y2": {
-                            "scale": "yscale",
-                            "value": 0
+                          'y2': {
+                            'scale': 'yscale',
+                            'value': 0
                           },
-                          "fill": {
-                            "value": "#4682B4"
+                          'fill': {
+                            'value': '#4682B4'
                           }
                         },
-                        "hover": {
-                          "fill": {
-                            "value": "red"
+                        'hover': {
+                          'fill': {
+                            'value': 'red'
                           }
                         }
                       }
                     },
                     {
-                      "type": "text",
-                      "encode": {
-                        "enter": {
-                          "align": {
-                            "value": "center"
+                      'type': 'text',
+                      'encode': {
+                        'enter': {
+                          'align': {
+                            'value': 'center'
                           },
-                          "baseline": {
-                            "value": "top"
+                          'baseline': {
+                            'value': 'top'
                           },
-                          "fill": {
-                            "value": "#000000"
+                          'fill': {
+                            'value': '#000000'
                           }
                         },
-                        "update": {
-                          "x": {
-                            "scale": "xscale",
-                            "signal": "tooltip.selectedCategory",
-                            "band": 0.5
+                        'update': {
+                          'x': {
+                            'scale': 'xscale',
+                            'signal': 'tooltip.selectedCategory',
+                            'band': 0.5
                           },
-                          "y": {
-                            "scale": "yscale",
-                            "signal": "tooltip.count",
-                            "offset": -12
+                          'y': {
+                            'scale': 'yscale',
+                            'signal': 'tooltip.count',
+                            'offset': -12
                           },
-                          "text": {
-                            "signal": "tooltip.count"
+                          'text': {
+                            'signal': 'tooltip.count'
                           },
-                          "fillOpacity": [
+                          'fillOpacity': [
                             {
-                              "test": "datum === tooltip",
-                              "value": 0
+                              'test': 'datum === tooltip',
+                              'value': 0
                             },
                             {
-                              "value": 1
+                              'value': 1
                             }
                           ]
                         }
                       }
                     }
                   ],
-                  "scales": [
+                  'scales': [
                     {
-                      "name": "xscale",
-                      "type": "band",
-                      "domain": {
-                        "data": "groupedBySelectedCategory",
-                        "field": "selectedCategory",
-                        "sort": True
+                      'name': 'xscale',
+                      'type': 'band',
+                      'domain': {
+                        'data': 'groupedBySelectedCategory',
+                        'field': 'selectedCategory',
+                        'sort': True
                       },
-                      "range": "width",
-                      "padding": 0.1,
-                      "round": True
+                      'range': 'width',
+                      'padding': 0.1,
+                      'round': True
                     },
                     {
-                      "name": "yscale",
-                      "domain": {
-                        "fields": [
+                      'name': 'yscale',
+                      'domain': {
+                        'fields': [
                           {
-                            "data": "groupedBySelectedCategory",
-                            "field": "count"
+                            'data': 'groupedBySelectedCategory',
+                            'field': 'count'
                           }
                         ]
                       },
-                      "nice": True,
-                      "range": [
+                      'nice': True,
+                      'range': [
                         {
-                          "signal": "chartHeight"
+                          'signal': 'chartHeight'
                         },
                         0
                       ]
                     }
                   ],
-                  "axes": [
+                  'axes': [
                     {
-                      "orient": "bottom",
-                      "scale": "xscale",
-                      "title": {
-                        "signal": "category"
+                      'orient': 'bottom',
+                      'scale': 'xscale',
+                      'title': {
+                        'signal': 'category'
                       },
-                      "labelAngle": 90,
-                      "labelAlign": "left",
-                      "labelBaseline": "middle"},
+                      'labelAngle': 90,
+                      'labelAlign': 'left',
+                      'labelBaseline': 'middle'},
                     {
-                      "orient": "left",
-                      "scale": "yscale",
-                      "title": "Number of Samples",
-                      "tickMinStep": 1
+                      'orient': 'left',
+                      'scale': 'yscale',
+                      'title': 'Number of Samples',
+                      'tickMinStep': 1
                     }
                   ]
                 }
