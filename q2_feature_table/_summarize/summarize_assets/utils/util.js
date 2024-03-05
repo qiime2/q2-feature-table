6907d99b1a04a3b6db0d0b8d36cccfd8ea9f05c8
Oddant1 <oddant1@hotmail.com>
2024-03-05 14:52:13 -0700

    Refactor sort

--- q2_feature_table/_summarize/summarize_assets/utils/util.js
+++ q2_feature_table/_summarize/summarize_assets/utils/util.js
@@ -5,7 +5,6 @@ const formatter = new Intl.NumberFormat({numberingSystem: "latn"})
 
 function formatTable(tableBody, data, sortedKeys) {
     for (const key of sortedKeys) {
-        console.log(key);
         let colIdx = 1;
         let row = tableBody.insertRow(0);
 
@@ -19,4 +18,16 @@ function formatTable(tableBody, data, sortedKeys) {
             dataCell.innerText = formatter.format(data[innerKey][key]);
         }
     }
+}
+
+
+function sortIDs(a, b, data) {
+    const diff = data[a] - data[b];
+    // if two samples have the same number of features then we
+    // determine the order using the sample ID alphabetical order
+    if (diff == 0){
+      return b.localeCompare(a);
+    }
+
+    return diff;
 }
