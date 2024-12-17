a8f0a54685f8f0a8d9d1cbdfba8dcd1d09495161
Greg Caporaso <greg.caporaso@nau.edu>
2024-12-17 12:39:42 -0700

    TST: adds subsample test that should have existed (prior to this PR)

--- q2_feature_table/tests/test_subsample.py
+++ q2_feature_table/tests/test_subsample.py
@@ -34,6 +34,19 @@ class SubsampleIDsTests(TestCase):
             npt.assert_equal(t.data(i, axis='sample'),
                              a.data(i, axis='sample'))
 
+    def test_subsample_samples_drop_empty_feature(self):
+        t = Table(np.array([[0, 0, 0], [1, 1, 2]]),
+                  ['O1', 'O2'],
+                  ['S1', 'S2', 'S3'])
+        a = subsample_ids(t, 2, 'sample')
+        self.assertEqual(a.shape, (1, 2))
+
+        sample_ids = frozenset(a.ids(axis='sample'))
+        self.assertIn(sample_ids, set([frozenset(['S1', 'S2']),
+                                       frozenset(['S1', 'S3']),
+                                       frozenset(['S2', 'S3'])]))
+        self.assertEqual(set(a.ids(axis='observation')), set(['O2']))
+
     def test_subsample_features(self):
         t = Table(np.array([[0, 1, 3], [1, 1, 2]]).T,
                   ['O1', 'O2', 'O3'],
@@ -50,6 +63,19 @@ class SubsampleIDsTests(TestCase):
         for i in a.ids(axis='observation'):
             npt.assert_equal(t.data(i, axis='observation'),
                              a.data(i, axis='observation'))
+
+    def test_subsample_features_drop_empty_samples(self):
+        t = Table(np.array([[0, 0, 0], [1, 1, 2]]).T,
+                  ['O1', 'O2', 'O3'],
+                  ['S1', 'S2'])
+        a = subsample_ids(t, 2, 'feature')
+        self.assertEqual(a.shape, (2, 1))
+
+        sample_ids = frozenset(a.ids(axis='observation'))
+        self.assertIn(sample_ids, set([frozenset(['O1', 'O2']),
+                                       frozenset(['O1', 'O3']),
+                                       frozenset(['O2', 'O3'])]))
+        self.assertEqual(set(a.ids(axis='sample')), set(['S2']))
 
     def test_subsample_samples_oversample(self):
         t = Table(np.array([[0, 1, 3], [1, 1, 2]]).T,
