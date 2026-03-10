c3fd74f6ddb8aa6e54c242d91ba4a49d6373c5a8
Macabe Wood <macabewood@gmail.com>
2026-03-10 12:07:27 -0700

    Cleaning up

--- q2_feature_table/_summarize/_visualizer.py
+++ q2_feature_table/_summarize/_visualizer.py
@@ -22,7 +22,6 @@ import skbio
 import qiime2
 import json
 from ._vega_spec import vega_spec
-from rachis.metadata import Metadata
 
 _blast_url_template = ("http://www.ncbi.nlm.nih.gov/BLAST/Blast.cgi?"
                        "ALIGNMENT_VIEW=Pairwise&PROGRAM=blastn&DATABASE"
@@ -64,11 +63,6 @@ def tabulate_seqs(output_dir: str, data: DNAIterator,
                          metadata_df[column].str.replace(',', '')).all()
                 ):
                     metadata_df[column] = numeric_column
-                else:
-                    metadata_df[column] = metadata_df[column]
-
-        metadata = Metadata(metadata_df)
-        metadata_df = metadata.to_dataframe()
 
         if merge_method == 'union':
             display_sequences = display_sequences.union(metadata_df.index)
