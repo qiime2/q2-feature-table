4db93f8b20f48921ad50fe7cea55653abc8ca2b0
Macabe Wood <macabewood@gmail.com>
2026-08-10 12:25:21 -0700

    small

--- q2_feature_table/tests/test_summarize.py
+++ q2_feature_table/tests/test_summarize.py
@@ -50,7 +50,6 @@ class TabulateSeqsTests(TestCase):
             self.assertTrue(os.path.exists(expected_fp))
             with open(expected_fp) as fh:
                 file_text = fh.read()
-                print(file_text)
                 self.assertTrue('ACGT' in file_text)
                 self.assertTrue('<td>4</td>' in file_text)
                 self.assertTrue('<td>seq2</td>' in file_text)
